b88548e57720e859d48d75d40a8a0fc3ab6effb0
W4lrus <filip.vodnansky@gmail.com>
2019-12-06 13:45:07 +0100

    Add files via upload
    Prepisovani Teymurova kodu pro nas pripad

---
+++ NNQvalues.py
@@ -0,0 +1,50 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch as T
+import numpy as np
+
+class Policy(nn.Module):
+    def __init__(self, client, hid_dim=64, tanh=False, std_fixed=True, obs_dim=None, act_dim=None):
+        super(Policy, self).__init__()
+
+        self.tanh = tanh
+        self.act_dim = 3
+
+        self.fc1 = nn.Linear(self.obs_dim, hid_dim)
+        self.m1 = nn.LayerNorm(hid_dim)
+        self.fc2 = nn.Linear(hid_dim, hid_dim)
+        self.m2 = nn.LayerNorm(hid_dim)
+        self.fc3 = nn.Linear(hid_dim, self.act_dim)
+
+        T.nn.init.kaiming_normal_(self.fc1.weight, mode='fan_in', nonlinearity='leaky_relu')
+        T.nn.init.kaiming_normal_(self.fc2.weight, mode='fan_in', nonlinearity='leaky_relu')
+        T.nn.init.kaiming_normal_(self.fc3.weight, mode='fan_in', nonlinearity='linear')
+
+        if std_fixed:
+            self.log_std = T.zeros(1, self.act_dim)
+        else:
+            self.log_std = nn.Parameter(T.zeros(1, self.act_dim))
+
+    def forward(self, x):
+        x = F.leaky_relu(self.m1(self.fc1(x)))
+        x = F.leaky_relu(self.m2(self.fc2(x)))
+        if self.tanh:
+            x = T.tanh(self.fc3(x))
+        else:
+            x = self.fc3(x)
+        return x
+
+    def sample_action(self, s):
+        return T.normal(self.forward(s), T.exp(self.log_std))
+
+    def log_probs(self, batch_states, batch_actions):
+        # Get action means from policy
+        action_means = self.forward(batch_states)
+
+        # Calculate probabilities
+        log_std_batch = self.log_std.expand_as(action_means)
+        std = T.exp(log_std_batch)
+        var = std.pow(2)
+        log_density = - T.pow(batch_actions - action_means, 2) / (2 * var) - 0.5 * np.log(2 * np.pi) - log_std_batch
+
+        return log_density.sum(1, keepdim=True)
---
+++ ppo.py
@@ -0,0 +1,186 @@
+import os
+import numpy as np
+import torch as T
+import my_utils
+import policies
+import random
+import string
+import socket
+import airsim
+import NNQvalues
+
+goal = [20, 20, 20]
+startdistance_goal = np.sqrt(goal[0]^2 + goal[1]^2 + goal[2]^2)
+
+
+def train(client, policy, params):
+    initX = 0
+    initY = 0
+    initZ = -10
+
+    policy_optim = T.optim.Adam(policy.parameters(), lr=params["policy_lr"], weight_decay=params["weight_decay"],
+                                eps=1e-4)
+
+    batch_states = []
+    batch_actions = []
+    batch_rewards = []
+    batch_new_states = []
+    batch_terminals = []
+    batch_ctr = 0
+    batch_rew = 0
+
+    step_ctr = 0
+
+    for i in range (params["iters"]):
+        # Start flying
+        client.takeoffAsync().join()
+        client.moveToPositionAsync(initX, initY, initZ, 5).join()
+
+        done = False
+        while not done:
+            # Sample action from policy
+            action = policy.sample_action(my_utils.to_tensor(client, True)).detach()
+
+            # Record transition
+            batch_states.append(my_utils.to_tensor(client.getMultirotorState().kinematics_estimated.position, True))
+            batch_actions.append(action)
+
+            # Step action
+            quad_vel = client.getMultirotorState().kinematics_estimated.linear_velocity
+            client.moveByVelocityAsync(quad_vel.x_val + action[0], quad_vel.y_val + action[1],
+                                       quad_vel.z_val + action[2], 5).join()
+
+            reward, done, _ = compute_reward()
+
+            batch_rew += reward
+
+            step_ctr += 1
+
+            # Record transition
+            batch_rewards.append(my_utils.to_tensor(np.asarray(reward, dtype=np.float32), True))
+            batch_new_states.append(my_utils.to_tensor(client.getMultirotorState().kinematics_estimated.position, True))
+            batch_terminals.append(done)
+
+        # Completed episode
+        batch_ctr += 1
+
+        if batch_ctr == params["batchsize"]:
+
+            batch_states = T.cat(batch_states)
+            batch_actions = T.cat(batch_actions)
+            batch_rewards = T.cat(batch_rewards)
+
+            # Scale rewards
+            batch_rewards = (batch_rewards - batch_rewards.mean()) / batch_rewards.std()
+
+            # Calculate episode advantages
+            batch_advantages = calc_advantages_MC(params["gamma"], batch_rewards, batch_terminals)
+
+            update_ppo(policy, policy_optim, batch_states, batch_actions, batch_advantages, params["ppo_update_iters"])
+
+            print("Episode {}/{}, loss_V: {}, loss_policy: {}, mean ep_rew: {}".
+                  format(i, params["iters"], None, None, batch_rew / params["batchsize"]))
+
+            # Finally reset all batch lists
+            batch_ctr = 0
+            batch_rew = 0
+
+            batch_states = []
+            batch_actions = []
+            batch_rewards = []
+            batch_new_states = []
+            batch_terminals = []
+
+        # Saved learned model
+        # if i % 100 == 0 and i > 0:
+        #     sdir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+        #                         "agents/{}_{}_{}_pg.p".format(env.__class__.__name__, policy.__class__.__name__, params["ID"]))
+        #     T.save(policy, sdir)
+        #     print("Saved checkpoint at {} with params {}".format(sdir, params))
+
+        # Reset
+        client.reset()
+        # If computer vision mode enabled
+        # # Reset position
+        # # currently reset() doesn't work in CV mode. Below is the workaround
+        # client.simSetVehiclePose(airsim.Pose(airsim.Vector3r(0, 0, 0), airsim.to_quaternion(0, 0, 0)), True)
+
+
+def update_ppo(policy, policy_optim, batch_states, batch_actions, batch_advantages, update_iters):
+    log_probs_old = policy.log_probs(batch_states, batch_actions).detach()
+    c_eps = 0.2
+
+    # Do ppo_update
+    for k in range(update_iters):
+        log_probs_new = policy.log_probs(batch_states, batch_actions)
+        r = T.exp(log_probs_new - log_probs_old)
+        loss = -T.mean(T.min(r * batch_advantages, r.clamp(1 - c_eps, 1 + c_eps) * batch_advantages))
+        policy_optim.zero_grad()
+        loss.backward()
+        policy.soft_clip_grads(3.)
+        policy_optim.step()
+
+
+def calc_advantages_MC(gamma, batch_rewards, batch_terminals):
+    N = len(batch_rewards)
+
+    # Monte carlo estimate of targets
+    targets = []
+    for i in range(N):
+        cumrew = T.tensor(0.)
+        for j in range(i, N):
+            cumrew += (gamma ** (j - i)) * batch_rewards[j]
+            if batch_terminals[j]:
+                break
+        targets.append(cumrew.view(1, 1))
+    targets = T.cat(targets)
+
+    return targets
+
+
+def transform_input(responses):
+    """Transforms output of simGetImages to one 84x84 image"""
+    img1d = np.array(responses[0].image_data_float, dtype=np.float)
+    img1d = 255/np.maximum(np.ones(img1d.size), img1d)
+    img2d = np.reshape(img1d, (responses[0].height, responses[0].width))
+
+    from PIL import Image
+    image = Image.fromarray(img2d)
+    im_final = np.array(image.resize((84, 84)).convert('L'))
+
+    return im_final
+
+
+def compute_reward(collision_info, client, previous_distance_goal):
+    # # distance from goal
+    # quad_state = client.getMultirotorState().kinematics_estimated.position
+    # collision_info = client.simGetCollisionInfo()
+    #
+    # distance_goal = np.sqrt((goal[0] - quad_state[0])^2 + (goal[1] - quad_state[1])^2 (goal[2] - quad_state[2])^2)
+    #
+    # reward = 0.1*(previous_distance_goal - distance_goal)
+    # if collision_info.has_collided:
+    #     reward -= 1000
+
+    reward = 0
+
+    return reward
+
+if __name__=="__main__":
+    # params = {"iters": 500000, "batchsize": 1, "gamma": 0.995, "policy_lr": 0.0007, "weight_decay": 0.0001, "ppo": True,
+    #           "ppo_update_iters": 6, "animate": False, "train": True}
+
+    params = {"iters": 100, "batchsize": 1, "gamma": 0.995, "policy_lr": 0.0007, "weight_decay": 0.0001, "ppo_update_iters": 6, "train": True}
+
+    # Airsim environment
+    client = airsim.MultirotorClient()
+    client.confirmConnection()
+    client.enableApiControl(True)
+    client.armDisarm(True)
+
+    policy = NNQValues.Policy(client)
+    train(client, policy, params)
+
+    # Quit
+    # that's enough fun for now. let's quit cleanly
+    client.enableApiControl(False)
