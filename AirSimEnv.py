4deab2d47534037958c3481d16213441ab443087
tichyt11 <tomtch7@gmail.com>
2019-12-10 00:11:30 +0100

    fixes

--- AirSimEnv.py
+++ AirSimEnv.py
@@ -1,11 +1,14 @@
 import airsim
-import numpy as np
 import torch as T
 import numpy as np
 
+'''drivetrain = airsim.DrivetrainType.ForwardOnly makes the drone move only forward.
+If drone is asked to move to side, he will first rotate, then move forward.
+drivetrain = airsim.DrivetrainType.MaxDegreeOfFreedom just moves drone in a specified direction without turning'''
 
-class AirSimEnv():
-    def __init__(self, takeoff=True, dt=0, freeze=False):
+
+class AirSimEnv:
+    def __init__(self, takeoff=True, dt=0, freeze=False):  # if freeze=True sim is frozen if possible
         self.client = airsim.MultirotorClient()   # connect to airsim simulator
         self.client.confirmConnection()
         self.client.enableApiControl(True)   # enable control over python
@@ -14,29 +17,37 @@ class AirSimEnv():
         self.freeze = freeze
         self.takeoff = takeoff
 
-        self.client.simPause(self.freeze)
-        self.state = self.get_obs() # init state
+        self.state = self.get_obs()  # init state
         if dt:
             self.drivetrain = airsim.DrivetrainType.ForwardOnly
         else:
             self.drivetrain = airsim.DrivetrainType.MaxDegreeOfFreedom
         if takeoff:
+            self.unfreeze()
             self.client.takeoffAsync().join()
+        self.client.simPause(self.freeze)
 
     def get_pos(self, numpy=True):
+        self.unfreeze()
         pos = self.client.simGetGroundTruthKinematics().position
+        self.client.simPause(self.freeze)
         if numpy:
-            pos = np.array([pos.x_val , pos.y_val , pos.z_val])
+            pos = np.array([pos.x_val, pos.y_val, pos.z_val])
         return pos
 
     def get_ori(self, numpy=True):
+        self.unfreeze()
         ori = self.client.simGetGroundTruthKinematics().orientation
+        self.client.simPause(self.freeze)
         if numpy:
             ori = np.array([ori.w_val, ori.x_val, ori.y_val, ori.z_val])
         return ori
 
     def get_rgb_img(self, tensor=True):  # return rgb image as numpy array or torch tensor
+        self.unfreeze()
         response = self.client.simGetImages([airsim.ImageRequest("0", airsim.ImageType.Scene, False, False)])[0]
+        self.client.simPause(self.freeze)
+
         img1d = np.frombuffer(response.image_data_uint8, dtype=np.uint8)
         img_rgb = img1d.reshape(response.height, response.width, 3)
         if tensor:
@@ -47,7 +58,10 @@ class AirSimEnv():
         return img_rgb
 
     def get_obs(self):  # return everything as numpy arrays
+        self.unfreeze()
         col = self.client.simGetCollisionInfo().has_collided
+        self.client.simPause(self.freeze)
+
         img_rgb = self.get_rgb_img(tensor=False)
         ori = self.get_ori()
         pos = self.get_pos()
@@ -55,36 +69,31 @@ class AirSimEnv():
         return self.state
 
     def reset(self):
+        self.unfreeze()
         self.client.reset()
         self.client.enableApiControl(True)  # enable control over python
         self.client.armDisarm(True)
 
-        self.client.simPause(self.freeze)
         self.state = self.get_obs()  # init state
         if self.takeoff:
             self.client.takeoffAsync().join()
+        self.client.simPause(self.freeze)
         return self.get_obs()
 
-    def step(self , vel_vector , duration = 1):  # sets drone velocity for duration in seconds
-        '''drivetrain = airsim.DrivetrainType.ForwardOnly makes the drone move only forward.
-        If drone is asked to move to side, he will first rotate, then move forward.
-        drivetrain = airsim.DrivetrainType.MaxDegreeOfFreedom just moves drone in a specified direction without turning'''
-
-        (vx, vy, vz) = vel_vector
-        self.unfreeze
-        self.client.moveByVelocityAsync(vx, vy, vz, duration, self.drivetrain).join()  #join makes the function wait for end of task
-        self.freeze
+    def step(self, vel_vector, duration=1):  # sets drone velocity for duration in seconds
+        self.unfreeze()
+        self.set_velocity_z(vel_vector, duration)
+        self.freeze()
         return self.get_obs()
 
     def step_z(self, vel_vector, duration=1):  # sets velocity using x,y vector and sets z constant
-        (vx, vy, z) = vel_vector
-        self.unfreeze
-        self.client.moveByVelocityZAsync(vx, vy, z, duration, self.drivetrain).join()
-        self.freeze
+        self.unfreeze()
+        self.set_velocity_z(vel_vector, duration)
+        self.freeze()
         return self.get_obs()
 
     def set_velocity(self, vel_vector, duration=1, wait=True):
-        self.unfreeze
+        self.unfreeze()
         (vx, vy, vz) = vel_vector
         if wait:
             self.client.moveByVelocityAsync(vx, vy, vz, duration, self.drivetrain).join()
@@ -92,23 +101,27 @@ class AirSimEnv():
             self.client.moveByVelocityAsync(vx, vy, vz, duration, self.drivetrain)
 
     def set_velocity_z(self, vel_vector, duration=1, wait=True):  # sets velocity using x,y vector and sets z constant
-        self.unfreeze
         (vx, vy, z) = vel_vector
         if wait:
             self.client.moveByVelocityZAsync(vx, vy, z, duration, self.drivetrain).join()
         else:
             self.client.moveByVelocityZAsync(vx, vy, z, duration, self.drivetrain)
+        print('no')
+
 
     def move_to(self , coordinates, v=5):
+        self.unfreeze()
         (x, y, z) = coordinates
         self.client.moveToPositionAsync(x, y, z, v).join()
+        self.client.simPause(self.freeze)
 
     def hover(self):
+        self.unfreeze()
         self.client.hoverAsync().join()
+        self.client.simPause(self.freeze)
 
     def freeze(self):
-        self.client.simPause(False)  # unfreeze
-
-    def unfreeze(self):
         self.client.simPause(True)  # unfreeze
 
+    def unfreeze(self):
+        self.client.simPause(False)  # unfreeze
